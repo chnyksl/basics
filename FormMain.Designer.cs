4c0eb258e4566044e91e455c6eb84522db1d34df
chnyksl <59435383+chnyksl@users.noreply.github.com>
2020-01-02 12:40:20 +0300

    Add files via upload

---
+++ FormMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EFaturaGoruntuleyici
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnYazdir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnYazdir);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSelectFile);
+            this.splitContainer1.Panel1.Controls.Add(this.txtFilePath);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1259, 730);
+            this.splitContainer1.SplitterDistance = 76;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 27);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(719, 22);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(737, 26);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(114, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "&Fatura Seç...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1259, 650);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.Location = new System.Drawing.Point(1133, 26);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(114, 23);
+            this.btnYazdir.TabIndex = 3;
+            this.btnYazdir.Text = "&Yazdır";
+            this.btnYazdir.UseVisualStyleBackColor = true;
+            this.btnYazdir.Click += new System.EventHandler(this.btnYazdir_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 730);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormMain";
+            this.Text = "E-Fatura Görüntüleme Aracı";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnYazdir;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace EFaturaGoruntuleyici
+{
+    public partial class FormMain : Form
+    {
+        Encoding en;
+        string strXLST = "";
+        private OpenFileDialog openFileDialog1;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            openFileDialog1 = new OpenFileDialog();
+            en = Encoding.UTF8;
+            string startup_path = Application.StartupPath;
+            string xlst_path = startup_path + "\\general.xslt";
+            if (!File.Exists(xlst_path))
+            {
+                MessageBox.Show(xlst_path + " dosyası bulunamadı");
+                return;
+            }
+            else
+            {
+                strXLST = File.ReadAllText(xlst_path, en);
+            }
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (!openFileDialog1.FileName.ToLower().EndsWith(".xml"))
+                    {
+                        MessageBox.Show("sadece xml uzantılı dosyalar desteklenmektedir");
+                        return;
+                    }
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    txtFilePath.Text = openFileDialog1.FileName;
+                    string strFaturaXML = sr.ReadToEnd();
+
+                    string ErrMsg = "";
+                    string strResult = Transform(strFaturaXML, strXLST, out ErrMsg);
+                    
+                    if (ErrMsg != "")
+                        MessageBox.Show(ErrMsg);
+                    else
+                    {
+                        webBrowser1.DocumentText = strResult;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error message: " + ex.Message + Environment.NewLine + "Details:" + ex.StackTrace);
+                }
+            }
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ShowPrintDialog();
+        }
+
+
+        private string Transform(string XMLPage, string XSLStylesheet, out string ErrorMessage)
+        {
+
+            string result = "";
+            ErrorMessage = "";
+            try
+            {
+                // Reading XML
+                TextReader textReader1 = new StringReader(XMLPage);
+                XmlTextReader xmlTextReader1 = new XmlTextReader(textReader1);
+                XPathDocument xPathDocument = new XPathDocument(xmlTextReader1);
+
+                //Reading XSLT
+                TextReader textReader2 = new StringReader(XSLStylesheet);
+                XmlTextReader xmlTextReader2 = new XmlTextReader(textReader2);
+
+                //Define XslCompiledTransform
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load(xmlTextReader2);
+
+
+                StringBuilder sb = new StringBuilder();
+                TextWriter tw = new StringWriter(sb);
+
+                xslt.Transform(xPathDocument, null, tw);
+
+                result = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+    }
+}
